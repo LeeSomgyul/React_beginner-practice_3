02b7576f44cc3588b7e63c35e435717e5c0302e6
LeeSomgyul <rmawl8600@naver.com>
2024-09-05 23:29:35 +0900

    0905 연습
    variants를 이용하여 정리하기.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {motion, spring} from "framer-motion";
+import {motion} from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -18,13 +18,15 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+  start: {scale: 0},
+  end: {scale: 1, rotateZ: 360, transition: {type: 'spring', bounce: 0.3, delay: 0.5}},
+}
+
 function App() {
   return (
     <Wrapper>
-      <Box 
-        initial={{scale: 0}}
-        animate={{scale: 1, rotateZ: 360}}
-        transition={{type: 'spring', bounce: 0.3, delay: 0.5}}>
+      <Box variants={myVars} initial="start" animate="end">
         &nbsp;{/*크롬에서 애니메이션이 작동하지 않는 버그를 해결하기 위한 공백 문자*/}
       </Box>
     </Wrapper>
