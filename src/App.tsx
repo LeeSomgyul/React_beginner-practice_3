659a66e9611ce4527cc115f906143affde3c22b9
LeeSomgyul <rmawl8600@naver.com>
2024-09-05 22:59:07 +0900

    0905 연습
    framer-motion으로 애니메이션 만들기(기초1)

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import {motion, spring} from "framer-motion";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(135deg, #6C48C5, #C68FE6);
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Box 
+        initial={{scale: 0}}
+        animate={{scale: 1, rotateZ: 360}}
+        transition={{type: 'spring', bounce: 0.3, delay: 0.5}}>
+        &nbsp;{/*크롬에서 애니메이션이 작동하지 않는 버그를 해결하기 위한 공백 문자*/}
+      </Box>
+    </Wrapper>
+  );
+}
+
+export default App;
