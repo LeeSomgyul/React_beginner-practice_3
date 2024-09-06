2b451b82824b80cc67d90724e07d384bc8f293d0
LeeSomgyul <rmawl8600@naver.com>
2024-09-06 16:16:05 +0900

    0906 연습
    framer-motion연습: drag기능 더 알아보기, ref활용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import {motion} from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -8,6 +9,17 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, #6C48C5, #C68FE6);
+`;
+
+const BiggerBox = styled(motion.div)`
+  width: 500px;
+  height: 500px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
 `;
 
 const Box = styled(motion.div)`
@@ -19,19 +31,30 @@ const Box = styled(motion.div)`
 `;
 
 const boxVariants = {
-  hover: {scale: 1.5, rotateZ: 90},
-  tap: {scale: 1, borderRadius: "100px"},
-  drag: {backgroundColor: "rgb(46, 204, 113)"},
+  hover: {rotateZ: 90},
+  tap: {borderRadius: "100px"},
 };
 
 
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);//처음에 아무것도 가르키지 않다가(null) 나중에 BiggerBox(div)를 참조한다는 의미
+
   return (
     <Wrapper>
-      <Box variants={boxVariants} drag whileHover="hover" whileTap="tap" whileDrag="drag">
-        &nbsp;
-      </Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box 
+          variants={boxVariants}
+          drag//drag기능 추가
+          dragSnapToOrigin//기존 위치로 돌아가려는 성질
+          dragConstraints={biggerBoxRef}//BiggerBox에서 Box가 벗어나지 못하도록
+          dragElastic={0.7}
+          whileHover="hover"
+          whileTap="tap"
+        >
+          &nbsp;
+        </Box>
+      </BiggerBox>
     </Wrapper>
   );
 }
