05792eb241d0deb297fa5aea5ebe0fb22f93a16d
LeeSomgyul <rmawl8600@naver.com>
2024-09-10 23:38:26 +0900

    0910 연습
    framer-motion의 AnimatePresence 연습(버튼 클릭 시 div가 슬라이드처럼 넘어가는 애니메이션)

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,17 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   background: linear-gradient(135deg, rgb(255, 120, 165), rgb(161, 64, 214));
 `;
 
 const Box = styled(motion.div)`
   width: 400px;
   height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   position: absolute;
@@ -24,42 +29,42 @@ const Box = styled(motion.div)`
 
 const boxVariants = {
   initial: {
+    x: 500,
     opacity: 0,
     scale: 0,
   },
   visivle: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    retateZ: 360,
   },
-  leaving: {
+  living: {
+    x: -500,
     opacity: 0,
     scale: 0,
-    y: 50,
   }
 }
 
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const handleClick = () => {
-    setShowing((prev) => !prev);
+  const [order, setOrder] = useState(1);
+  const handleNext = () => {
+    setOrder((prev) => (prev === 10 ? 10 : prev + 1));
   }
 
  
   return (
     <Wrapper>
-      <button onClick={handleClick}>클릭!</button>
       <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visivle"
-            exit="leaving"
-          />
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => 
+          i === order ? (
+            <Box variants={boxVariants} initial="initial" animate="visivle" exit="living" key={i}>
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
+      <button onClick={handleNext}>클릭!</button>
     </Wrapper>
   );
 }
