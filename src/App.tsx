32892988fc7b6109e1b934bdafb62e725c25dbe6
LeeSomgyul <rmawl8600@naver.com>
2024-09-13 11:12:05 +0900

    0913 연습
    framer-motion의 layoutId연습(gird에서 특정 박스 클릭 시 선택 박스 가운데로 오는 애니메이션)

--- src/App.tsx
+++ src/App.tsx
@@ -12,46 +12,49 @@ const Wrapper = styled(motion.div)`
   background: linear-gradient(135deg, rgb(255, 120, 165), rgb(161, 64, 214));
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child{
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
-  width: 300px;
-  height: 300px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  height: 200px;
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  margin: 0px 50px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  height: 100px;
-  width: 100px;
 `;
 
 
 function App() {
-  const [click, setClick] = useState(false);
-  const switchPosition = () => {
-    setClick((prev) => (!prev));
-  }
- 
+  const [boxId, setBoxId] = useState<null|string>(null);
+  
   return (
     <Wrapper>
+      <Grid>
+        {["1", "2", "3", "4"].map(i=><Box key={i} layoutId={i} onClick={() => setBoxId(i)}/>)}
+      </Grid>
       <AnimatePresence>
-        <Box>
-          {click ? (
-            <Circle layoutId="circle" style={{borderRadius: 50}}/>
-          ) : null}
-        </Box>
-        <Box>
-          {!click ? (
-            <Circle layoutId="circle" style={{borderRadius: 0, scale: 2}}/>
-          ) : null}
-        </Box>
+        {boxId ? 
+          <Overlay onClick={() => setBoxId(null)} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
+            <Box layoutId={boxId} style={{width: 450, height: 250}}/>
+          </Overlay>
+        : null};
       </AnimatePresence>
-      <button onClick={switchPosition}>클릭!</button>
     </Wrapper>
   );
 }
